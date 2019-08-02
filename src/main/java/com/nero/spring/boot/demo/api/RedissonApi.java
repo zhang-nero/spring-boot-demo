6bf4c54b38bd34aa12d8af583f002b65727e67d1
Nero <zhangao@anmav.com>
2019-08-02 18:55:27 +0800

    加入jedis配置

--- src/main/java/com/nero/spring/boot/demo/api/RedissonApi.java
+++ src/main/java/com/nero/spring/boot/demo/api/RedissonApi.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.JedisPool;
 
 import java.util.concurrent.TimeUnit;
 
@@ -50,5 +51,14 @@ public class RedissonApi {
         }
 
         return result;
+    }
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    @ApiOperation(value = "redis测试", httpMethod = "GET", notes = "redis测试")
+    @RequestMapping(value = "redisTest", method = RequestMethod.GET)
+    public void redisTest(){
+
     }
 }
--- src/main/java/com/nero/spring/boot/demo/config/RedisConfig.java
+++ src/main/java/com/nero/spring/boot/demo/config/RedisConfig.java
@@ -1,13 +1,13 @@
 package com.nero.spring.boot.demo.config;
 
 import org.redisson.Redisson;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author Nero
@@ -17,20 +17,39 @@ import org.springframework.context.annotation.Configuration;
 public class RedisConfig {
     @Value("${redis.host}")
     private String host;
-    @Value("${redisson.database}")
-    private Integer database;
-    @Value("${redisson.password}")
+    @Value("${redis.port}")
+    private int port;
+    @Value("${redis.password}")
     private String password;
+    @Value("${redis.database}")
+    private Integer redisDatabase;
+
+    @Value("${redisson.database}")
+    private Integer redissonDatabase;
 
     @Bean
     public RedissonClient redissonClient(){
 
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://" + host)
+                .setAddress("redis://" + host + ":" + port)
                 .setPassword(password)
-                .setDatabase(database);
+                .setDatabase(redissonDatabase);
         RedissonClient redissonClient = Redisson.create(config);
         return redissonClient;
+    }
+
+    @Bean
+    public JedisPool getJedisPool(){
+
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMinIdle(5);
+        jedisPoolConfig.setMaxIdle(10);
+        jedisPoolConfig.setMaxWaitMillis(10000);
+        jedisPoolConfig.setMaxTotal(50);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, 5000, password, redisDatabase);
+
+        return jedisPool;
     }
 }
